b16d2ce2f197323ef9822935d054281e2b2b6934
mrkiranvaku-debug <mrkiranvaku@gmail.com>
2026-09-25 23:36:02 +0530

    FEATURE: Added teacher repository layer

---
+++ backend/student-management-backend/src/main/java/com/studentmanagement/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.studentmanagement.repository;
+
+import com.studentmanagement.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+    Teacher findByTeacherCode(String teacherCode);
+}
